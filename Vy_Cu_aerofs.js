df47a7612c64decf2c6811dfb30093f141391824
veeweeherman <vy.hoang.cu@gmail.com>
2015-10-06 16:36:54 -0700

    first commit

---
+++ Vy_Cu_aerofs.js
@@ -0,0 +1,21 @@
+// input: search type, sorted array, key/target
+// output: match type, index of found match
+
+// check if input is correct: is the array actually an array? is it full of integers?
+// check which input search type: LessThan, LessThanEquals, Equals, GreaterThan, GreaterThanEquals
+// is the sorted array ascending or descending?
+
+// linear time and place
+
+var findItem = function(searchType, array, key){
+  if (Array.isArray(array)) {
+    console.log('hello');
+  } else {
+    return "you need to input an array!";
+  }
+}
+var func = function() {
+  console.log(kkkk)
+}
+console.log( 3+8 );
+console.log(findItem(1,{},3))
